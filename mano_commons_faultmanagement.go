30da084128ce56c599560cbf2b24601bdd65c975
Marco Cilloni <m.c.cilloni@gmail.com>
2016-12-19 12:00:52 +0100

    Splitted fault management types from commons

---
+++ mano_commons_faultmanagement.go
@@ -0,0 +1,12 @@
+package openbaton
+
+type FaultManagementAction string
+
+const (
+	FaultRestart              FaultManagementAction = "RESTART"
+	FaultReinstantiateService FaultManagementAction = "REINSTANTIATE_SERVICE"
+	FaultHeal                 FaultManagementAction = "HEAL"
+	FaultReinstantiate        FaultManagementAction = "REINSTANTIATE"
+	FaultSwitchToStandby      FaultManagementAction = "SWITCH_TO_STANDBY"
+	FaultSwitchToActive       FaultManagementAction = "SWITCH_TO_ACTIVE"
+)
