8fa9374ecb44bb784fe6e63386c7d59af68cc3f5
Marco Cilloni <m.c.cilloni@gmail.com>
2016-12-20 19:32:14 +0100

    Adding more types

--- catalogue/messages/nfvo_or_messages.go
+++ catalogue/messages/nfvo_or_messages.go
@@ -1,1 +1,37 @@
 package messages
+
+type OrAllocateResources struct {
+
+}
+
+type OrError struct {
+
+}
+
+type OrGeneric struct {
+
+}
+
+type OrGrantLifecycleOperation struct {
+
+}
+
+type OrHealVNFRequest struct {
+
+}
+
+type OrInstantiate struct {
+
+}
+
+type OrScaling struct {
+
+}
+
+type OrStartStop struct {
+
+}
+
+type OrUpdate struct {
+    
+}
