920baa9a18b8dceb3fbfdbfb5f32de180c067d16
Marco Cilloni <m.c.cilloni@gmail.com>
2017-02-05 14:09:54 +0100

    Added copyright to files

--- vnfm/config/config.go
+++ vnfm/config/config.go
@@ -1,4 +1,20 @@
 /*
+ *  Copyright (c) 2017 Open Baton (http://openbaton.org)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
 Package config parses a VNFM config as defined by a TOML file.
 This config file can specify both generical vnfm parameters (under the [vnfm] section) and driver-specific configurations;
 the package will only read the [vnfm] section into a Config structure, leaving all the other parameters available to be read from a Properties instance.
